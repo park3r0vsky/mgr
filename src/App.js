bb94bd3367e4ea6c4671391f059773de15eb32c9
park3r0vsky <spenser96@o2.pl>
2022-04-21 02:48:38 +0200

    fixed a lot of bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import {useEffect, useState} from 'react'
 import * as React from "react"
 import './App.css'
@@ -13,14 +12,14 @@ const App = () => {
   [
     'https://i.imgur.com/eU4Ww4q.png',
     'https://i.imgur.com/cA6vwIw.png',
+    'https://i.imgur.com/MzHoavD.png',
   ]
 
   const [account, setAccount] = useState(null)
   const [token, setToken] = useState(null)
   const [totalSupply, setTotalSupply] = useState(null)
-  const [name, setName] = useState(null)
   const [tokenURIs, setTokenURIs] = useState([])
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState(null)
 
 
   const loadWeb3 = async () => {
@@ -53,7 +52,7 @@ const App = () => {
       setToken(token)
       const totalSupply = await token.methods.totalSupply().call()
       const name = await token.methods.name().call()
-      setTotalSupply(totalSupply)
+      setTotalSupply(parseInt(totalSupply))
 
       let balanceOf = await token.methods.balanceOf(accounts[0]).call()
       for (let i = 0; i < balanceOf; i++) {
@@ -78,8 +77,6 @@ const App = () => {
     }
 
     const mint_nft = (_numberNFT) => {
-    //let numberNFT = Math.floor(Math.random() * URIs.length)
-    setCount(count => count + 1)
     token.methods.mint(
       account,
       uris[_numberNFT]
@@ -88,6 +85,7 @@ const App = () => {
     .on('transactionHash', (hash) => {
 
       setTokenURIs(tokenURIs => [...tokenURIs, uris[_numberNFT]])
+      setTotalSupply(totalSupply => totalSupply + 1)
 
 
     })
@@ -117,33 +115,35 @@ const App = () => {
     k.loadRoot('https://i.imgur.com/')
     k.loadSprite('player-left', 'pPVBL7J.png')
     k.loadSprite('player-right', 'hI9GDGw.png')
-    k.loadSprite('player-down', 'ftihTbH.png')
     k.loadSprite('player-up', 'pZqjg0n.png')
+    k.loadSprite('player-down', 'ftihTbH.png')
     k.loadSprite('left-wall', 'h9n6MIc.png')
+    k.loadSprite('right-wall', 'aRkFWGk.png')
     k.loadSprite('top-wall', '6lIeFmb.png')
     k.loadSprite('bottom-wall', '6lIeFmb.png')
-    k.loadSprite('right-wall', 'aRkFWGk.png')
-    k.loadSprite('bottom-left-wall', 'FD4OV5s.png')
-    k.loadSprite('bottom-right-wall', 'SWumN86.png')
     k.loadSprite('top-left-wall', 'q33puFy.png')
     k.loadSprite('top-right-wall', 'QcPWqbe.png')
-    k.loadSprite('top-door', 'gLssH5h.png')
-    k.loadSprite('lava-tile', 'nHhAWLn.png')
-    k.loadSprite('torch-tile', '8jNYA6F.png')
-    k.loadSprite('left-stairs', 'PKPSZp2.png')
-    k.loadSprite('up-torch', 'FQrRYdg.png')
+    k.loadSprite('bottom-left-wall', 'FD4OV5s.png')
+    k.loadSprite('bottom-right-wall', 'SWumN86.png')
     k.loadSprite('left-torch', 'CZHf5o6.png')
     k.loadSprite('right-torch', 'tApKS22.png')
-    k.loadSprite('ghost', '0MGVEwZ.png')
+    k.loadSprite('up-torch', 'FQrRYdg.png')
+    //k.loadSprite('floor-tile', 'LUup1AG.png')
+    k.loadSprite('lava-tile', 'nHhAWLn.png')
+    k.loadSprite('torch-tile', '8jNYA6F.png')
+    k.loadSprite('door-top', 'gLssH5h.png')
+    k.loadSprite('door-left', 'PKxiYtK.png')
+    k.loadSprite('left-stairs', 'PKPSZp2.png')
+    k.loadSprite('ghost-left', '0MGVEwZ.png')
     k.loadSprite('ghost-right', 'ysbpe2o.png')
-    k.loadSprite('wanderer', 'CBxQtFS.png')
+    k.loadSprite('wanderer-down', 'CBxQtFS.png')
     k.loadSprite('wanderer-up', 'unFX5f9.png')
     k.loadSprite('tornado', '6iSGfpL.png')
-    k.loadSprite('kaboom', 'o9WizfI.png')
+    k.loadSprite('charged-bolt', 'hsrj8Pg.png')
     k.loadSprite('stairs', 'bC3dBbM.png')
     k.loadSprite('chest-closed', '5tdiIIx.png')
     k.loadSprite('chest-opened', 'WliBmsB.png')
-    k.loadSprite('bg', 'VxMUetX.png')
+    k.loadSprite('bg', '7vRv1J3.png')
 
     k.play("soundtrack", {
         volume: 0.8,
@@ -156,7 +156,7 @@ const App = () => {
 
       const maps = [
        [
-         'yc)ccc^c)cw',
+         'wc)ccc^c)cx',
          'a         b',
          'a      *  b',
          'a   (     b',
@@ -164,18 +164,29 @@ const App = () => {
          'a     (   b',
          'a  ( *    b',
          'a         b',
-         'xd)ddddd)dz',
+         'yd)ddddd)dz',
        ],
        [
-         'ycccccccccw',
+         'wcccccccccx',
          'a       e b',
          '<         >',
+         'a    }    b',
+         '%         b',
          'a         b',
+         '<     $   >',
          'a         b',
-         'a    $    b',
-         '<   }     >',
-         'a         b',
-         'xdddddddddz',
+         'ydddddddddz',
+       ],
+       [
+         'wcccccccccx',
+         'a       e b',
+         '<   *     >',
+         'a    } !  b',
+         '?   *     b',
+         'a    ! }  b',
+         '<  $      >',
+         'a     *   b',
+         'ydddddddddz',
        ],
      ]
 
@@ -187,20 +198,22 @@ const App = () => {
         'b': ()  => [k.sprite('right-wall'), k.area(), k.solid(), 'wall'],
         'c': ()  => [k.sprite('top-wall'), k.area(), k.solid(), 'wall'],
         'd': ()  => [k.sprite('bottom-wall'), k.area(), k.solid(), 'wall'],
-        'w': ()  => [k.sprite('top-right-wall'), k.area(), k.solid(), 'wall'],
-        'x': ()  => [k.sprite('bottom-left-wall'), k.area(), k.solid(), 'wall'],
-        'y': ()  => [k.sprite('top-left-wall'), k.area(), k.solid(), 'wall'],
+        'w': ()  => [k.sprite('top-left-wall'), k.area(), k.solid(), 'wall'],
+        'x': ()  => [k.sprite('top-right-wall'), k.area(), k.solid(), 'wall'],
+        'y': ()  => [k.sprite('bottom-left-wall'), k.area(), k.solid(), 'wall'],
         'z': ()  => [k.sprite('bottom-right-wall'), k.area(), k.solid(), 'wall'],
-        '%': ()  => [k.sprite('left-stairs'), k.area(), k.solid(), 'door'],
-        '^': ()  => [k.sprite('top-door'), k.area(), 'next-level' ],
-        '$': ()  => [k.sprite('stairs'), k.area(), 'next-level' ],
-        '*': ()  => [k.sprite('ghost'), k.area(), 'ghost', 'dangerous', { dir: -1} ],
-        '}': ()  => [k.sprite('wanderer'), k.area(), 'wanderer', 'dangerous', { dir: -1, timer: 0 } ],
-        ')': ()  => [k.sprite('up-torch'), k.area(), k.solid()],
         '<': ()  => [k.sprite('left-torch'), k.area(), k.solid(), 'wall'],
         '>': ()  => [k.sprite('right-torch'), k.area(), k.solid(), 'wall'],
+        '?': ()  => [k.sprite('door-left'), k.area(), k.solid(), 'wall'],
+        '%': ()  => [k.sprite('left-stairs'), k.area(), k.solid(), 'door'],
+        '^': ()  => [k.sprite('door-top'), k.area(), 'next-level' ],
+        '$': ()  => [k.sprite('stairs'), k.area(), 'next-level' ],
+        ')': ()  => [k.sprite('up-torch'), k.area(), k.solid()],
         '(': ()  => [k.sprite('torch-tile'), k.area(), k.solid()],
+        '!': ()  => [k.sprite('lava-tile'), k.area(), k.solid(), 'wall'],
         'e': ()  => [k.sprite('chest-closed'), k.area(), k.solid(), 'chest-closed'],
+        '*': ()  => [k.sprite('ghost-left'), k.area(), 'ghost', 'mob', 'dangerous', { dir: -1} ],
+        '}': ()  => [k.sprite('wanderer-down'), k.area(), 'wanderer', 'mob', 'dangerous', k.solid(), { dir: -1, timer: 0 } ],
       }
 
       k.addLevel(maps[level], levelCfg)
@@ -235,13 +248,13 @@ const App = () => {
 
         k.go("game", {
           level: (level +1) % maps.length,
-          score: scoreLabel.text,
+          score: scoreLabel.value,
         })
       })
 
       player.onCollide('chest-closed', (c) => {
 
-        const obj = k.add([
+          k.add([
             k.sprite("chest-opened"),
             k.pos(c.pos),
             'chest-opened',
@@ -251,12 +264,12 @@ const App = () => {
 
           let numberNFT = Math.floor(Math.random() * uris.length)
           thisGameNFTs.push(uris[numberNFT])
-          console.log(thisGameNFTs)
+
           mint_nft(numberNFT)
 
           c.destroy()
           k.play("chest-open", {volume: 0.4})
-          console.log('collide')
+
       })
 
       k.onKeyDown('left', () => {
@@ -265,8 +278,6 @@ const App = () => {
         )
         player.move(-PLAYER_SPEED, 0)
         player.dir = k.vec2(-1, 0)
-        scoreLabel.value++
-        scoreLabel.text = scoreLabel.value
       })
 
       k.onKeyDown('right', () => {
@@ -294,6 +305,11 @@ const App = () => {
       })
 
 
+      k.onKeyPress('q', () => {
+        k.destroyAll("mob")
+        k.shake(6)
+      })
+
       k.onKeyPress('s', () => {
         if (tokenURIs.includes(uris[0]) || thisGameNFTs.includes(uris[0])){
            PLAYER_SPEED = 270
@@ -308,18 +324,56 @@ const App = () => {
       })
 
       k.onKeyPress('space', () => {
-        spawnTornado(player.pos.add(player.dir.scale(48)))
-        k.play("explosion", {volume: 0.5})
+        spawnChargedBolt(player)
       })
 
 
-      function spawnTornado(p) {
-        const obj = k.add([k.sprite('tornado'), k.pos(p), 'tornado', k.area()])
-        k.wait(5, () => {
-            k.destroy(obj)
-        })
+      function spawnChargedBolt(p) {
+        const obj = k.add([
+          k.sprite('charged-bolt'),
+          k.pos(p.pos.add(p.dir.scale(48))),
+          'charged-bolt',
+          'player-attack',
+          k.area(),
+          k.move(p.dir,150),
+          ])
+        k.wait(1, () => {
+              k.destroy(obj)
+          })
       }
 
+      function spawnTornado(p, speed) {
+        const obj = k.add([
+          k.sprite('tornado'),
+          k.pos(p.pos),
+          'tornado',
+          'dangerous',
+          k.area(),
+          k.move(p.dir, speed),
+        ])
+      }
+
+
+      k.onCollide('player-attack', 'wall', (a,w) =>{
+        k.shake(4)
+        k.destroy(a)
+        k.play("explosion", {volume: 0.5})
+      })
+
+      k.onCollide('tornado', 'wall', (t,w) =>{
+        k.shake(1)
+        k.destroy(t)
+        //k.play("explosion", {volume: 0.5})
+      })
+
+      k.onCollide('player-attack', 'mob', (a,m) =>{
+        k.shake(2)
+        k.destroy(a)
+        k.destroy(m)
+        k.play("explosion", {volume: 0.2})
+        scoreLabel.value++
+        scoreLabel.text = 'score: '+ scoreLabel.value
+      })
 
         //Ghost MOB
         const GHOST_SPEED = 150
@@ -328,14 +382,14 @@ const App = () => {
           s.move(s.dir * GHOST_SPEED, 0)
         })
 
-        k.onCollide('ghost', 'wall', (s) => {
-          s.dir = -s.dir
-          if (s.dir > 0) {
-            s.use(k.sprite('ghost-right'))
+        k.onCollide('ghost', 'wall', (g) => {
+          g.dir = -g.dir
+          if (g.dir > 0) {
+            g.use(k.sprite('ghost-right'))
 
           }
           else{
-            s.use(k.sprite('ghost'))
+            g.use(k.sprite('ghost-left'))
           }
 
         })
@@ -343,36 +397,52 @@ const App = () => {
 
         //Wanderer MOB
         const WANDERER_SPEED = 60
+        const TORNADO_SPEEDS = [150,-170,200,-120]
 
-        k.onUpdate('wanderer', (s) => {
-          s.move(0, s.dir * WANDERER_SPEED)
-          s.timer -= k.dt()
-          if (s.timer <= 0) {
-            //spawnTornado(s)
-            s.dir = -s.dir
+        k.onUpdate('wanderer', (w) => {
+          w.move(0, w.dir * WANDERER_SPEED)
+          w.timer -= k.dt()
+          if (w.timer <= 0) {
+            w.dir = -w.dir
+            let numberSpeed = Math.floor(Math.random() * TORNADO_SPEEDS.length)
+            spawnTornado(w , TORNADO_SPEEDS[numberSpeed])
 
-            if (s.dir > 0) {
-              s.use(k.sprite('wanderer'))
+            if (w.dir > 0) {
+              w.use(k.sprite('wanderer-down'))
+
             }
             else {
-              s.use(k.sprite('wanderer-up'))
+              w.use(k.sprite('wanderer-up'))
+
             }
-            s.timer = k.rand(5)
+            w.timer = k.rand(5)
           }
         })
 
-        k.onCollide('wanderer', 'wall', (s) => {
-          s.dir = -s.dir
+        k.onCollide('wanderer', 'wall', (w) => {
+          w.dir = -w.dir
 
-          if (s.dir > 0) {
-            s.use(k.sprite('wanderer'))
+          if (w.dir > 0) {
+            w.use(k.sprite('wanderer-down'))
           }
           else{
-            s.use(k.sprite('wanderer-up'))
+            w.use(k.sprite('wanderer-up'))
           }
         })
 
+      player.onCollide('dangerous', () => {
+        k.go('lose', {score : scoreLabel.value})
+      })
+
     })
+
+    k.scene("lose", ({ score }) => {
+      k.add([k.text('GAME OVER',{size: 25}), k.origin('center'), k.pos(264,150)])
+      k.add([k.text('SCORE:' + score), k.origin('center'), k.pos(k.width()/2, k.height()/2)])
+      k.add([k.text('REFRESH TO TRY AGAIN', {size: 25} ), k.origin('center'), k.pos(264,450)])
+
+    })
+
 
   k.go('game', { level: 0, score: 0 })
 
@@ -388,7 +458,7 @@ if (name == null) {
   return (
     <div>
       <div id="load-text">
-        <h1>Deploy Smart Contract and Log In</h1>
+        <h1>Deploy Smart Contract and Connect Wallet</h1>
       </div>
       <div id="load-emojis">
         <h1>📝🗝️🔥</h1>
